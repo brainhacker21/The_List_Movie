6012f95745867584af7082f66f0b15f8cb4d771d
Auric <myemail>
2022-09-29 10:44:54 +0700

    fix testing

--- androidcore/src/test/java/com/auric/themoviessub1byauric/androidcore/ExampleUnitTest.kt
+++ androidcore/src/test/java/com/auric/themoviessub1byauric/androidcore/ExampleUnitTest.kt
@@ -2,7 +2,7 @@ package com.auric.themoviessub1byauric.androidcore
 
 import org.junit.Test
 
-import org.junit.Assert.
+import org.junit.Assert.assertEquals
 
 /**
  * Example local unit test, which will execute on the development machine (host).
